bef26870e5aa63bf10391af0b8036af3c6b39927
Christopher Bourke <cbourke@cse.unl.edu>
2015-10-26 17:49:22 -0500

    redacted password (and changed it)

--- src/unl/cse/albums/DatabaseInfo.java
+++ src/unl/cse/albums/DatabaseInfo.java
@@ -2,7 +2,7 @@ package unl.cse.albums;
 
 public class DatabaseInfo {
 
-	public static final String url = "jdbc:mysql://cse.unl.edu/cbourke";
-	public static final String username = "cbourke";
-	public static final String password = "eowPx4";
+	public static final String url = "jdbc:mysql://cse.unl.edu/login";
+	public static final String username = "login";
+	public static final String password = "password";
 }
